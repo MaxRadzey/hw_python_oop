95bdf2c9c02a2384ff1e6a14518722328867a93f
Макисм Радзей <max.radzey@yandex.ru>
2023-08-17 19:01:29 +0300

    version3.1

--- homework.py
+++ homework.py
@@ -136,8 +136,7 @@ def main(training: Training) -> None:
 if __name__ == '__main__':
     packages = [('SWM', [720, 1, 80, 25, 40]),
                 ('RUN', [15000, 1, 75]),
-                ('WLK', [9000, 1, 75, 180]),
-                ('aaa', [1,2,3,4,5])]
+                ('WLK', [9000, 1, 75, 180])]
     try:
         for workout_type, data in packages:
             training = read_package(workout_type, data)
